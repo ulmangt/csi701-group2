5bef2443d72dd620f8a02dc3e6606ad3d7c79293
geoff.ulman <geoff.ulman@46f05b90-4def-d416-28b4-2990c0d3ae30>
2010-10-20 04:46:13 +0000

    populate-db: added code to perform very simple calculation on handwriting data set and store the results back to the database.

--- populate-db/clojure/src/analyze_db_clojure/core.clj
+++ populate-db/clojure/src/analyze_db_clojure/core.clj
@@ -6,5 +6,38 @@
            [java.util.zip GZIPInputStream]
            [java.util Arrays]))
 
+;
+; Example command to calculate mean intensities for each sample by character type
+; and store the results back to the database
+;
+; (do
+;   (populate-db-mean-intensity (deref (analyze-mean-intensity "0")))
+;   (populate-db-mean-intensity (deref (analyze-mean-intensity "1")))
+;   (populate-db-mean-intensity (deref (analyze-mean-intensity "2")))
+;   (populate-db-mean-intensity (deref (analyze-mean-intensity "3")))
+;   (populate-db-mean-intensity (deref (analyze-mean-intensity "4")))
+;   (populate-db-mean-intensity (deref (analyze-mean-intensity "5")))
+;   (populate-db-mean-intensity (deref (analyze-mean-intensity "6")))
+;   (populate-db-mean-intensity (deref (analyze-mean-intensity "7")))
+;   (populate-db-mean-intensity (deref (analyze-mean-intensity "8")))
+;   (populate-db-mean-intensity (deref (analyze-mean-intensity "9"))))
 
+(defn populate-db-mean-intensity [result-map]
+  (with-connection db
+    (apply insert-rows "Handwriting.Result" (map #(vector nil (first %) "MeanIntensity" (second %)) result-map))))
+
+(defn calculate-mean-intensity [rs result-map]
+  (let [ixData (:ixdata rs)
+        bData  (signed-to-unsigned-array (:bdata rs))
+        sum    (apply + bData)
+        size   (alength bData)
+        mean   (float (/ sum size))]
+    (swap! result-map assoc ixData mean)))
+
+(defn analyze-mean-intensity [character]
+  (let [result-map (atom {})]
+    (with-connection db
+      (with-query-results rs ["SELECT * FROM Handwriting.Data WHERE Data.sCharacter = ?" character]
+        (doall (map calculate-mean-intensity rs (repeat result-map)))))
+    result-map))
 
