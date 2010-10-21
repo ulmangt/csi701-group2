0f648026bac1430caf322ee06f167112e67c8b56
geoff.ulman <geoff.ulman@46f05b90-4def-d416-28b4-2990c0d3ae30>
2010-10-21 01:36:27 +0000

    handwriting-viewer: added framework for rcp based database viewer application.

---
+++ handwriting-viewer/src/edu/gmu/csi/view/DataListView.java
@@ -0,0 +1,22 @@
+package edu.gmu.csi.view;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class DataListView extends ViewPart {
+	public static final String ID = "handwriting-viewer.datalistview";
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
