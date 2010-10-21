99d3455a21e06a0fa3c1ab43fed952895ca2cfa3
geoff.ulman <geoff.ulman@46f05b90-4def-d416-28b4-2990c0d3ae30>
2010-10-21 01:45:44 +0000

    handwriting-viewer: added shell of DataListView (to view text/hierarchical representation of database objects.

--- trunk/handwriting-viewer/src/edu/gmu/csi/view/DataListView.java
+++ trunk/handwriting-viewer/src/edu/gmu/csi/view/DataListView.java
@@ -1,22 +1,93 @@
 package edu.gmu.csi.view;
 
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
 public class DataListView extends ViewPart {
 	public static final String ID = "handwriting-viewer.datalistview";
 
-	@Override
-	public void createPartControl(Composite parent) {
-		// TODO Auto-generated method stub
+	private TreeViewer treeViewer;
+	
+	public DataListView( )
+	{
 		
 	}
-
+	
+	@Override
+	public void createPartControl(Composite parent) {
+		treeViewer = new TreeViewer(parent);
+		
+		treeViewer.setContentProvider(new DataListContentProvider());
+		treeViewer.setLabelProvider(new DataListLabelProvider());
+		treeViewer.setInput(getInitialInput());
+		
+		treeViewer.expandAll();
+	}	
+	
 	@Override
 	public void setFocus() {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	private Object getInitialInput( )
+	{
+		return null;
+	}
+	
+	private class DataListContentProvider implements ITreeContentProvider
+	{
 
+		@Override
+		public void dispose() {
+			// TODO Auto-generated method stub
+			
+		}
 
+		@Override
+		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public Object[] getElements(Object inputElement) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Object[] getChildren(Object parentElement) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Object getParent(Object element) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public boolean hasChildren(Object element) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+	}
+	
+	private class DataListLabelProvider extends LabelProvider
+	{
+		public Image getImage(Object element) {
+			return null;
+		}
+
+		public String getText(Object element) {
+			return element == null ? "" : element.toString();
+		}
+	}
 }
