1c9dcf748a93d7e4b7e8248f73483305d6007b83
geoff.ulman <geoff.ulman@46f05b90-4def-d416-28b4-2990c0d3ae30>
2010-10-25 01:32:27 +0000

    handwriting-viewer: added shell of handwriting view.

--- handwriting-viewer/src/edu/gmu/csi/Perspective.java
+++ handwriting-viewer/src/edu/gmu/csi/Perspective.java
@@ -5,6 +5,7 @@ import org.eclipse.ui.IPerspectiveFactory;
 
 import edu.gmu.csi.view.CharacterImageView;
 import edu.gmu.csi.view.DataListView;
+import edu.gmu.csi.view.ResultListView;
 
 public class Perspective implements IPerspectiveFactory
 {
@@ -15,7 +16,8 @@ public class Perspective implements IPerspectiveFactory
 		layout.setFixed( false );
 
 		layout.addView( DataListView.ID, IPageLayout.LEFT, 0.8f, layout.getEditorArea( ) );
-		layout.addView( CharacterImageView.ID, IPageLayout.RIGHT, 0.2f, DataListView.ID );
+		layout.addView( CharacterImageView.ID, IPageLayout.RIGHT, 0.3f, DataListView.ID );
+		layout.addView( ResultListView.ID, IPageLayout.BOTTOM, 0.8f, DataListView.ID );
 
 	}
 
