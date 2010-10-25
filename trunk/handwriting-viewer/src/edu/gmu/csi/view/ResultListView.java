5bbb9b2b58b3b6ba4722ec5f99ba0b6f953d770a
geoff.ulman <geoff.ulman@46f05b90-4def-d416-28b4-2990c0d3ae30>
2010-10-25 01:32:27 +0000

    handwriting-viewer: added shell of handwriting view.

---
+++ trunk/handwriting-viewer/src/edu/gmu/csi/view/ResultListView.java
@@ -0,0 +1,24 @@
+package edu.gmu.csi.view;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class ResultListView extends ViewPart
+{
+	public static final String ID = "handwriting-viewer.resultlistview";
+
+	@Override
+	public void createPartControl( Composite parent )
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setFocus( )
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
