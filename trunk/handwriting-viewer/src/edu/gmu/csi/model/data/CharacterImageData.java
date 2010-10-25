7d4a879cbf658b6f1df3b148c75fbe5828db0b26
geoff.ulman <geoff.ulman@46f05b90-4def-d416-28b4-2990c0d3ae30>
2010-10-25 00:46:11 +0000

    handwriting-viewer: added capability to display multiple samples simultaneously.

---
+++ trunk/handwriting-viewer/src/edu/gmu/csi/model/data/CharacterImageData.java
@@ -0,0 +1,42 @@
+package edu.gmu.csi.model.data;
+
+import org.eclipse.swt.graphics.Image;
+
+import edu.gmu.csi.model.Data;
+
+public class CharacterImageData
+{
+	private CharacterData data;
+	private Image image;
+	
+	public CharacterImageData( CharacterData data, Image image )
+	{
+		this.data = data;
+		this.image = image;
+	}
+	
+	public Data getData( )
+	{
+		return data.getData( );
+	}
+	
+	public byte[] getImageData( )
+	{
+		return data.getImageData( );
+	}
+
+	public Image getImage( )
+	{
+		return image;
+	}
+
+	public int getImageRows( )
+	{
+		return data.getData( ).getRows( );
+	}
+
+	public int getImageColumns( )
+	{
+		return data.getData( ).getCols( );
+	}
+}
