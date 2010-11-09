9f27b40660f6bc9b94c6633a006330f57f110dbe
geoff.ulman <geoff.ulman@46f05b90-4def-d416-28b4-2990c0d3ae30>
2010-11-09 22:44:59 +0000

    handwriting-viewer: added simple skeleton of ConfusionMatrixView.

---
+++ handwriting-viewer/src/edu/gmu/csi/view/ConfusionMatrixView.java
@@ -0,0 +1,25 @@
+package edu.gmu.csi.view;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class ConfusionMatrixView extends ViewPart
+{
+	public static final String ID = "handwriting-viewer.confusionmatrixview";
+
+	@Override
+	public void createPartControl( Composite parent )
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setFocus( )
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
