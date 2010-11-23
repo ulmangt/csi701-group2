2c7f014b2a1c45d95c75740759ae6d8305a03a55
geoff.ulman <geoff.ulman@46f05b90-4def-d416-28b4-2990c0d3ae30>
2010-11-23 21:57:52 +0000

    handwriting-viewer: updated viewer to read database connection information from configuration file.

--- handwriting-viewer/src/edu/gmu/csi/manager/DatabaseManager.java
+++ handwriting-viewer/src/edu/gmu/csi/manager/DatabaseManager.java
@@ -10,23 +10,58 @@ import org.apache.commons.dbcp.PoolableConnectionFactory;
 import org.apache.commons.dbcp.PoolingDriver;
 import org.apache.commons.pool.ObjectPool;
 import org.apache.commons.pool.impl.GenericObjectPool;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.osgi.service.prefs.BackingStoreException;
+import org.osgi.service.prefs.Preferences;
 
 public class DatabaseManager
 {
 	private static final DatabaseManager instance = new DatabaseManager( );
 
-	private static final String host = "ec2-67-202-7-152.compute-1.amazonaws.com";
-	private static final int port = 3306;
-	private static final String user = "test";
-	private static final String pass = "csi710";
+	private static final String defaultHost = "ec2-67-202-7-152.compute-1.amazonaws.com";
+	private static final String defaultPort = "3306";
+	private static final String defaultUser = "test";
+	private static final String defaultPass = "csi710";
 	
 	public DatabaseManager( )
 	{
+		String host = defaultHost;
+		String port = defaultPort;
+		String user = defaultUser;
+		String pass = defaultPass;
+		
+		Preferences nodeCore = new InstanceScope().getNode("handwriting-viewer");
+		System.out.println( nodeCore );
+		if (nodeCore != null) {
+			
+			host = nodeCore.get("database.host", defaultHost);
+			port = nodeCore.get("database.port", defaultPort);
+			user = nodeCore.get("database.user", defaultUser);
+			pass = nodeCore.get("database.pass", defaultPass);
+			
+			System.out.println( host + " " + port + " " + user + " " + pass );
+		
+			nodeCore.put("database.host", host);
+			nodeCore.put("database.port", port);
+			nodeCore.put("database.user", user);
+			nodeCore.put("database.pass", pass);
+
+			try
+			{
+				nodeCore.flush( );
+				nodeCore.sync( );
+			}
+			catch ( BackingStoreException e )
+			{
+				e.printStackTrace();
+			}
+		}
+		
 		try
 		{
 			// see example at http://svn.apache.org/viewvc/commons/proper/dbcp/trunk/doc/ManualPoolingDriverExample.java?view=markup
 			ObjectPool connectionPool = new GenericObjectPool(null);
-			ConnectionFactory connectionFactory = new DriverManagerConnectionFactory( String.format( "jdbc:mysql://%s:%d", host, port ), user, pass );
+			ConnectionFactory connectionFactory = new DriverManagerConnectionFactory( String.format( "jdbc:mysql://%s:%s", host, port ), user, pass );
 			new PoolableConnectionFactory(connectionFactory,connectionPool,null,null,false,true);
 			Class.forName("org.apache.commons.dbcp.PoolingDriver");
 			PoolingDriver driver = (PoolingDriver) DriverManager.getDriver("jdbc:apache:commons:dbcp:");
