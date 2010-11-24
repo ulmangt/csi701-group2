226b4f676b58d0503867b5a914434ab0526fa82a
geoff.ulman <geoff.ulman@46f05b90-4def-d416-28b4-2990c0d3ae30>
2010-11-24 06:11:33 +0000

    populate-db: added more queries.

--- populate-db/scripts/queries.sql
+++ populate-db/scripts/queries.sql
@@ -2,13 +2,13 @@
 
 -- QUESTION 1 Which digit yielded the best accuracy of detection by our tests?
 
--- select number of correctly classified samples per run and character type
+-- select number of correctly classified samples (per run and character type)
 (select Result.ixRun, Data.sCharacter, count(*) as count from Result, Data where Result.ixData = Data.ixData and Result.sClassification = Data.sCharacter group by Result.ixRun, Data.sCharacter) as Correct;
 
--- select total number of samples per run and character type
+-- select total number of samples (per run and character type)
 (select Result.ixRun, Data.sCharacter, count(*) as count from Result, Data where Result.ixData = Data.ixData group by Result.ixRun, Data.sCharacter) as Total;
 
--- select percentage of correctly classified samples per run and character type
+-- select percentage of correctly classified samples (per run and character type)
 select Total.ixRun, Total.sCharacter, Correct.count/Total.count from 
   (select Result.ixRun, Data.sCharacter, count(*) as count from Result, Data where Result.ixData = Data.ixData group by Result.ixRun, Data.sCharacter) as Total,
   (select Result.ixRun, Data.sCharacter, count(*) as count from Result, Data where Result.ixData = Data.ixData and Result.sClassification = Data.sCharacter group by Result.ixRun, Data.sCharacter) as Correct
@@ -99,7 +99,7 @@ select Total.ixRun, Total.sCharacter, Correct.count/Total.count from
 | 201011232 | 9          |                    0.3803 |
 +-----------+------------+---------------------------+
 
--- select character type with largest percentage of correctly classified samples per run
+-- select character type with largest percentage of correctly classified samples (per run)
 select * from (
 select Total.ixRun, Total.sCharacter, Correct.count/Total.count as accuracy from 
   (select Result.ixRun, Data.sCharacter, count(*) as count from Result, Data where Result.ixData = Data.ixData group by Result.ixRun, Data.sCharacter) as Total,
@@ -181,11 +181,132 @@ select count(*), convert(sValue,UNSIGNED INTEGER) as age from Data, Metadata whe
 |       10 |   57 |
 +----------+------+
 
--- select the number of samples with particular age values which were correctly classified
-select ixRun, convert(sValue,UNSIGNED INTEGER) as age, count(*) from Data, Metadata, Result where Data.ixData = Metadata.ixData and Data.ixData = Result.ixData and Data.sCharacter = Result.sClassification and Metadata.sKey = 'Age' and Data.ixDataSet = 2 group by Result.ixRun, age;
+-- select the number of samples with particular age values (per run)
+select Result.ixRun, AgeData.sValue, count(*) from Result, (select Metadata.ixData, Metadata.sValue, Data.sCharacter from Metadata, Data where Metadata.ixData = Data.ixData and Metadata.sKey = 'Age') as AgeData where AgeData.ixData = Result.ixData group by Result.ixRun, AgeData.sValue;
 
-select ixRun, convert(sValue,UNSIGNED INTEGER) as age from Data, Metadata, Result where Data.ixData = Metadata.ixData and Data.ixData = Result.ixData and Data.sCharacter = Result.sClassification and Metadata.sKey = 'Age' and Data.ixDataSet = 2;
-select ixRun, sValue from Data, Metadata, Result where Data.ixData = Metadata.ixData and Data.ixData = Result.ixData and Data.sCharacter = Result.sClassification and Metadata.sKey = 'Age' and Data.ixDataSet = 2;
+-- select the number of samples with particular age values which were correctly classified (per run)
+select Result.ixRun, AgeData.sValue, count(*) from Result, (select Metadata.ixData, Metadata.sValue, Data.sCharacter from Metadata, Data where Metadata.ixData = Data.ixData and Metadata.sKey = 'Age') as AgeData where AgeData.ixData = Result.ixData and AgeData.sCharacter = Result.sClassification group by Result.ixRun, AgeData.sValue;
 
-select Result.ixRun, count(*) from Result, (select Metadata.ixData, Metadata.sValue, Data.sCharacter from Metadata, Data where Metadata.ixData = Data.ixData and Metadata.sKey = 'Age') as AgeData where AgeData.ixData = Result.ixData group by Result.ixRun;
+-- select percentage of correctly classified samples (per run and age value)
+select Total.ixRun, Total.sValue as age, Correct.count/Total.count as accuracy from 
+  (select Result.ixRun, AgeData.sValue, count(*) as count from Result, (select Metadata.ixData, Metadata.sValue, Data.sCharacter from Metadata, Data where Metadata.ixData = Data.ixData and Metadata.sKey = 'Age') as AgeData where AgeData.ixData = Result.ixData group by Result.ixRun, AgeData.sValue) as Total,
+  (select Result.ixRun, AgeData.sValue, count(*) as count from Result, (select Metadata.ixData, Metadata.sValue, Data.sCharacter from Metadata, Data where Metadata.ixData = Data.ixData and Metadata.sKey = 'Age') as AgeData where AgeData.ixData = Result.ixData and AgeData.sCharacter = Result.sClassification group by Result.ixRun, AgeData.sValue) as Correct
+where Total.ixRun = Correct.ixRun and Total.sValue = Correct.sValue order by Total.ixRun, accuracy desc;
+
++-----------+------+----------+
+| ixRun     | age  | accuracy |
++-----------+------+----------+
+| 201011231 | 6    |   0.8000 |
+| 201011231 | 7    |   0.6813 |
+| 201011231 | 9    |   0.4932 |
+| 201011231 | 8    |   0.4379 |
+| 201011231 | 13   |   0.3000 |
+| 201011231 | 10   |   0.2278 |
+| 201011231 | 16   |   0.1444 |
+| 201011231 | 47   |   0.1333 |
+| 201011231 | 18   |   0.1333 |
+| 201011231 | 14   |   0.1200 |
+| 201011231 | 15   |   0.1143 |
+| 201011231 | 11   |   0.1125 |
+| 201011231 | 17   |   0.1091 |
+| 201011231 | 56   |   0.1000 |
+| 201011231 | 49   |   0.1000 |
+| 201011231 | 39   |   0.1000 |
+| 201011231 | 57   |   0.1000 |
+| 201011231 | 53   |   0.1000 |
+| 201011231 | 48   |   0.1000 |
+| 201011231 | 45   |   0.1000 |
+| 201011231 | 19   |   0.1000 |
+| 201011232 | 6    |   1.0000 |
+| 201011232 | 56   |   1.0000 |
+| 201011232 | 7    |   0.8969 |
+| 201011232 | 9    |   0.6479 |
+| 201011232 | 8    |   0.6103 |
+| 201011232 | 13   |   0.6000 |
+| 201011232 | 39   |   0.4000 |
+| 201011232 | 45   |   0.4000 |
+| 201011232 | 10   |   0.3926 |
+| 201011232 | 47   |   0.3917 |
+| 201011232 | 15   |   0.3429 |
+| 201011232 | 11   |   0.3250 |
+| 201011232 | 14   |   0.2800 |
+| 201011232 | 48   |   0.2667 |
+| 201011232 | 18   |   0.2333 |
+| 201011232 | 17   |   0.2182 |
+| 201011232 | 16   |   0.2111 |
+| 201011232 | 57   |   0.2000 |
+| 201011232 | 19   |   0.2000 |
+| 201011232 | 49   |   0.1000 |
+| 201011232 | 53   |   0.1000 |
++-----------+------+----------+
+
+-- select percentage of correctly classified samples (per run and age value)
+-- restrict results to those age groups where we have larger than 100 data samples
+select Total.ixRun, Total.sValue as age, Correct.count/Total.count as accuracy from 
+  (select Result.ixRun, AgeData.sValue, count(*) as count from Result, (select Metadata.ixData, Metadata.sValue, Data.sCharacter from Metadata, Data where Metadata.ixData = Data.ixData and Metadata.sKey = 'Age') as AgeData where AgeData.ixData = Result.ixData group by Result.ixRun, AgeData.sValue) as Total,
+  (select Result.ixRun, AgeData.sValue, count(*) as count from Result, (select Metadata.ixData, Metadata.sValue, Data.sCharacter from Metadata, Data where Metadata.ixData = Data.ixData and Metadata.sKey = 'Age') as AgeData where AgeData.ixData = Result.ixData and AgeData.sCharacter = Result.sClassification group by Result.ixRun, AgeData.sValue) as Correct
+where Total.ixRun = Correct.ixRun and Total.sValue = Correct.sValue and Total.count > 100 order by Total.ixRun, accuracy desc;
+
++-----------+------+----------+
+| ixRun     | age  | accuracy |
++-----------+------+----------+
+| 201011231 | 7    |   0.6813 |
+| 201011231 | 9    |   0.4932 |
+| 201011231 | 8    |   0.4379 |
+| 201011231 | 10   |   0.2278 |
+| 201011231 | 47   |   0.1333 |
+| 201011231 | 14   |   0.1200 |
+| 201011231 | 17   |   0.1091 |
+| 201011232 | 7    |   0.8969 |
+| 201011232 | 9    |   0.6479 |
+| 201011232 | 8    |   0.6103 |
+| 201011232 | 10   |   0.3926 |
+| 201011232 | 47   |   0.3917 |
+| 201011232 | 14   |   0.2800 |
+| 201011232 | 17   |   0.2182 |
++-----------+------+----------+
+
+
+-------------------------------------------------------------------------------------------
+
+-- QUESTION 6 What is the accuracy of detection from collected data based on handedness?
+
+select Total.ixRun, Total.sValue as handedness, Correct.count/Total.count as accuracy from 
+  (select Result.ixRun, HandData.sValue, count(*) as count from Result, (select Metadata.ixData, Metadata.sValue, Data.sCharacter from Metadata, Data where Metadata.ixData = Data.ixData and Metadata.sKey = 'Handedness') as HandData where HandData.ixData = Result.ixData group by Result.ixRun, HandData.sValue) as Total,
+  (select Result.ixRun, HandData.sValue, count(*) as count from Result, (select Metadata.ixData, Metadata.sValue, Data.sCharacter from Metadata, Data where Metadata.ixData = Data.ixData and Metadata.sKey = 'Handedness') as HandData where HandData.ixData = Result.ixData and HandData.sCharacter = Result.sClassification group by Result.ixRun, HandData.sValue) as Correct
+where Total.ixRun = Correct.ixRun and Total.sValue = Correct.sValue order by Total.ixRun, accuracy desc;
+
++-----------+------------+----------+
+| ixRun     | handedness | accuracy |
++-----------+------------+----------+
+| 201011231 | L          |   0.3778 |
+| 201011231 | R          |   0.3487 |
+| 201011231 | B          |   0.1000 |
+| 201011232 | L          |   0.5556 |
+| 201011232 | R          |   0.5195 |
+| 201011232 | B          |   0.3000 |
++-----------+------------+----------+
+
+-------------------------------------------------------------------------------------------
+
+-- QUESTION 7 Is there a difference in accuracy of the algorithms on data samples visually classified as "good" or "bad" by a team member?
+
+select Total.ixRun, Total.sValue as quality, Correct.count/Total.count as accuracy from 
+  (select Result.ixRun, QualityData.sValue, count(*) as count from Result, (select Metadata.ixData, Metadata.sValue, Data.sCharacter from Metadata, Data where Metadata.ixData = Data.ixData and Metadata.sKey = 'Quality') as QualityData where QualityData.ixData = Result.ixData group by Result.ixRun, QualityData.sValue) as Total,
+  (select Result.ixRun, QualityData.sValue, count(*) as count from Result, (select Metadata.ixData, Metadata.sValue, Data.sCharacter from Metadata, Data where Metadata.ixData = Data.ixData and Metadata.sKey = 'Quality') as QualityData where QualityData.ixData = Result.ixData and QualityData.sCharacter = Result.sClassification group by Result.ixRun, QualityData.sValue) as Correct
+where Total.ixRun = Correct.ixRun and Total.sValue = Correct.sValue order by Total.ixRun, accuracy desc;
+
++-----------+---------+----------+
+| ixRun     | quality | accuracy |
++-----------+---------+----------+
+| 201011231 | B       |   0.4932 |
+| 201011231 | G       |   0.3264 |
+| 201011232 | B       |   0.6159 |
+| 201011232 | G       |   0.5061 |
++-----------+---------+----------+
+
+-------------------------------------------------------------------------------------------
+
+-- QUESTION 9 Are there any common miscategorizations for sets of digits (such as classifying 6's and 9's as 0's)?
+
 
